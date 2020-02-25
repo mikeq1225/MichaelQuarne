e9d50dc4de177a1a41e2fecd2a281c616827c139
Michael Quarne <mikedq1225@gmail.com>
2020-02-24 21:56:35 -0800

    added socialBar

---
+++ src/components/SocialBar.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "../styles/SocialBar.css"
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaGithub
+} from "react-icons/fa"
+
+export default props => {
+  return (
+    <div className="social">
+      <a href="https://github.com/mikeq1225" target="_blank" rel="noopener">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/michaelquarne/"
+        target="_blank"
+        rel="noopener"
+      >
+        <FaLinkedinIn />
+      </a>
+      <a href="https://www.facebook.com/mquarne" target="_blank" rel="noopener">
+        <FaFacebookF />
+      </a>
+      <a
+        href="https://www.instagram.com/trulyqsstyle/"
+        target="_blank"
+        rel="noopener"
+      >
+        <FaInstagram />
+      </a>
+    </div>
+  )
+}
