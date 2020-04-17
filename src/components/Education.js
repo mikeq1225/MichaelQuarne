61d87d228dc3805670b6af1169f04701b2546ef6
MikeQ <mikedq1225@gmail.com>
2020-04-17 15:27:08 -0700

    started education component

---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/Education.scss"
+import { Link } from "react-router-dom"
+
+export default props => {
+    return (
+        <div className="educationContainer">
+            <Link to={"/Portfolio-Mike-Quarne"}>
+                <button className="homeButton">Home</button>
+            </Link>
+        </div>
+    )
+}
