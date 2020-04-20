28e57560317e3cdfb83cb0e9c6519c3bb8345984
MikeQ <mikedq1225@gmail.com>
2020-04-20 11:11:12 -0700

    added project placeholders

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,68 @@ import { Link } from "react-router-dom"
 
 
 export default props => {
-    return (
-        <div className="projectsContainer">
-            <Link to={"/Portfolio-Mike-Quarne"}>
-                <button className="homeButton">Home</button>
-            </Link>
+  return (
+    <div className="projectsContainer">
+      <h3>Projects</h3>
+      <div className="projects">
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
         </div>
-    )
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Ska8er-Site</p>
+          <p>Description: </p>
+          <p>Problems faced:</p>
+          <p>Lessons Learned:</p>
+         </div>
+        </div>
+      </div>
+      <Link to={"/Portfolio-Mike-Quarne"}>
+        <button className="homeButton">Home</button>
+      </Link>
+    </div>
+  )
 }
