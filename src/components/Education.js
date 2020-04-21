5370741c4476c060b3977220c2fa3431ccb0f42c
MikeQ <mikedq1225@gmail.com>
2020-04-21 15:34:19 -0700

    added to education page

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,20 +3,22 @@ import "../styles/Education.scss"
 import { Link } from "react-router-dom"
 
 export default props => {
-    return (
-        <div className="educationContainer">
-            <div>
-                <h2>PunchCode</h2>
-                <p> Certification: Full-Stack Software Engineer Program</p>
-            </div>
-            <div>
-                <h2>Southern New Hampshire University</h2>
-                <p> Degree: Associate of Science</p>
-                <p> Field: Information Technologies with Highest Honors</p>
-            </div>
-            <Link to={"/Portfolio-Mike-Quarne"}>
-                <button className="homeButton">Home</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="educationContainer">
+      <div>
+        <h2>PunchCode</h2>
+        <p> Certification: Full-Stack Software Engineer Program</p>
+        <p>Description: PunchCode is a 3 month fulltime coding bootcamp where I learned more than just how to write code.  I completed 420 hours of education, experiences, performances, and examinations to earn my Certificate.</p>
+      </div>
+      <div>
+        <h2>Southern New Hampshire University</h2>
+        <p> Degree: Associate of Science</p>
+        <p> Program: Information Technologies with Highest Honors</p>
+        <p>Description:  I enrolled in the online Associate degree program while I was employeed at Breakthru Beverage.  I was working full time and doing the online classes fulltime.  It took about a year and a half to complete the program, but it was well worth it.  I graduated with a 4.0 GPA and Highest Honors. </p>
+      </div>
+      <Link to={"/Portfolio-Mike-Quarne"}>
+        <button className="homeButton">Home</button>
+      </Link>
+    </div>
+  )
 }
