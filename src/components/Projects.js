17582a2d2247f84c64af25dbefaaff5c97a4257e
MikeQ <mikedq1225@gmail.com>
2020-04-22 09:49:04 -0700

    added half of project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,9 +12,27 @@ export default props => {
          <img src="http://placehold.it/50" alt="Project picture"/>
          <div>
           <p>Title: Ska8er-Site</p>
-          <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
+          <p>Description:  Our first project in class.  We were given an image of a website and we had to recreate the site.  It was meant to teach us principles of HTML and CSS. </p>
+          <p>Problems:  Trying to figure out how to position things in the correct space and order.</p>
+          <p>Lessons:  I learned how to use CSS Grid and Flexbox quite well.  Also learned how to use pseudo elements.</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Create-Etsy-Search-Page</p>
+          <p>Description: Given a mock-up of an Etsy page, we had to recreate the page.  We were also given a JSON file with dummy data to be used for the search.</p>
+          <p>Problems: How to dynamically import the data without typing all of it out.</p>
+          <p>Lessons Learned: This was our first step into JavaScript.  I also learned how to use jQuery to create a tmeplate to map through and display the data.</p>
+         </div>
+        </div>
+        <div>
+         <img src="http://placehold.it/50" alt="Project picture"/>
+         <div>
+          <p>Title: Group-Project</p>
+          <p>Description: A group assingment where we utilized Github for version control and collaboration to make a website for a fictional restaurant.  We were given a JSON file with menu items.</p>
+          <p>Problems:  How do we work on the same project without causing problems and still creating a cohesive looking website.</p>
+          <p>Lessons:  I learned how to use Github's branches to ensure my team could work on the same website but in different sections to avoid conflicts.</p>
          </div>
         </div>
         <div>
@@ -22,8 +40,8 @@ export default props => {
          <div>
           <p>Title: Ska8er-Site</p>
           <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
+          <p>Problems:</p>
+          <p>Lessons:</p>
          </div>
         </div>
         <div>
@@ -31,8 +49,8 @@ export default props => {
          <div>
           <p>Title: Ska8er-Site</p>
           <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
+          <p>Problems:</p>
+          <p>Lessons:</p>
          </div>
         </div>
         <div>
@@ -40,26 +58,8 @@ export default props => {
          <div>
           <p>Title: Ska8er-Site</p>
           <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
-         </div>
-        </div>
-        <div>
-         <img src="http://placehold.it/50" alt="Project picture"/>
-         <div>
-          <p>Title: Ska8er-Site</p>
-          <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
-         </div>
-        </div>
-        <div>
-         <img src="http://placehold.it/50" alt="Project picture"/>
-         <div>
-          <p>Title: Ska8er-Site</p>
-          <p>Description: </p>
-          <p>Problems faced:</p>
-          <p>Lessons Learned:</p>
+          <p>Problems:</p>
+          <p>Lessons:</p>
          </div>
         </div>
       </div>
