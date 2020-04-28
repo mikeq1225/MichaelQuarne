bef76434134f95a1ca8952158d956487321c02d0
Michael Quarne <mikedq1225@gmail.com>
2020-04-28 11:24:44 -0700

    tried react tranisition group to no avail

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,34 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Home"
 import AboutMe from "./AboutMe"
-import Projects from "./Projects"
+import Projects from "./projects/Projects"
+import SkaterSite from "./projects/SkaterSite"
 import Education from "./Education"
 import SocialBar from "./SocialBar"
 import NavBar from "./NavBar"
 
-export default props => {
-  return (
-    <Router>
-      <div>
-        <Route exact path="/Portfolio-Mike-Quarne" component={Home}></Route>
-        <Route path="/Portfolio-Mike-Quarne" component={SocialBar}></Route>
-        <Route path="/Portfolio-Mike-Quarne" component={NavBar}></Route>
-        <Route path="/Portfolio-Mike-Quarne/About" component={AboutMe}></Route>
-        <Route path="/Portfolio-Mike-Quarne/Projects" component={Projects}></Route>
-        <Route path="/Portfolio-Mike-Quarne/Education" component={Education}></Route>
-      </div>
-    </Router>
-  )
+export default (props) => {
+	return (
+		<Router>
+			<div>
+				<Route exact path="/Portfolio-Mike-Quarne" component={Home}></Route>
+				<Route path="/Portfolio-Mike-Quarne" component={SocialBar}></Route>
+				<Route path="/Portfolio-Mike-Quarne" component={NavBar}></Route>
+				<Route path="/Portfolio-Mike-Quarne/About" component={AboutMe}></Route>
+				<Route
+					exact
+					path="/Portfolio-Mike-Quarne/Projects"
+					component={Projects}
+				></Route>
+				<Route
+					path="/Portfolio-Mike-Quarne/Education"
+					component={Education}
+				></Route>
+				<Route
+					path="/Portfolio-Mike-Quarne/Projects/Ska8er-Site"
+					component={SkaterSite}
+				></Route>
+			</div>
+		</Router>
+	)
 }
