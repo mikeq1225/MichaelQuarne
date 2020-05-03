d983631f94021c1bf28faec9dc4e9f2fe83a34c3
Michael Quarne <mikedq1225@gmail.com>
2020-05-02 17:20:59 -0700

    added react player to about me component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ReactPlayer from "react-player"
 import "../styles/AboutMe.scss"
 import { Link } from "react-router-dom"
 
@@ -7,36 +8,45 @@ export default (props) => {
 		<div className="container">
 			<div className="aboutMe">
 				<div>
-					<h3>
-						I'm a new full-stack software engineer from Las Vegas. I recently
-						graduated from PunchCode and am continually looking for new
-						opportunitites to show off my skills and learn new ones.
-					</h3>
-					<h1>Hello, I'm Mike Quarne.</h1>
-					<p>
-						Head over to my portfolio to see some of the projects I did while I
-						was in school.
-					</p>
-					{/* Insert youTube video of my video resume */}
-				</div>
-				<div className="infoAboutMe">
-					<div>
-						<h3>Family</h3>
-						<img
-							src="http://placehold.it/200"
-							alt="Family of Mike Quarne"
-						></img>
-						<img
-							src="http://placehold.it/200"
-							alt="Family2 of Mike Quarne"
-						></img>
+					<div className="playerWrapper">
+						<ReactPlayer
+							className="reactPlayer"
+							url="https://www.youtube.com/watch?v=XxrOahK4cDE&t=2s"
+							controls={true}
+						/>
 					</div>
+					<p>
+						Michael Quarne is a full-stack web developer from Las Vegas. A
+						recent graduate from PunchCode’s three month long Full-Stack
+						Engineer bootcamp. Skilled at building web applications from the
+						ground up – from concept, to design, to writing well-designed,
+						testable and efficient code while focusing on UX/UI. The completion
+						of Michael’s capstone project, a vacation planning web application
+						called Jurn(ease), showcases how much he has learned in three
+						months.
+					</p>
+					<p>
+						Michael’s coding philosophy starts with strategic planning,
+						designing, and developing applications to help reach desired
+						objectives and goals. He surrounds himself with technical people,
+						activities, hobbies, and endeavors with a drive to keep learning
+						through experiences.
+					</p>
+					<p>
+						Originally from Milwaukee, WI, Michael and his wife relocated to Las
+						Vegas after their children joined the military and informed them
+						that they would not be moving back to Wisconsin. Michael and his
+						wife decided it was a good time to move somewhere warm and exciting
+						to begin a new chapter in their lives.
+					</p>
+				</div>
+				{/* <div className="infoAboutMe">
 					<div>
 						<h3>Free Time</h3>
 						<p>hobbies</p>
 						<p>interests</p>
 					</div>
-				</div>
+				</div> */}
 				<Link to={"/Portfolio-Mike-Quarne"}>
 					<button className="homeButton">Home</button>
 				</Link>
